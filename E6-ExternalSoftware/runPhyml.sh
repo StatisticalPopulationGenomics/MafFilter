9ac49f4976ea1658af5b97cd46d05396c188f405
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2018-10-02 10:07:40 +0200

    Fixed typos in scripts.

--- E6-ExternalSoftware/runPhyml.sh
+++ E6-ExternalSoftware/runPhyml.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-phyml -i blockIn.phy -d nt -q -m HKY85 -f m -t e -c 4 -a e -s BEST -o tlr -b 0 > phyml.log 2> pĥyml.err
+phyml -i blockIn.phy -d nt -q -m HKY85 -f m -t e -c 4 -a e -s BEST -o tlr -b 0 > phyml.log 2> phyml.err
 
